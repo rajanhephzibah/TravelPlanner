208b425dfe7cbd7bdb632069b6d1b51899ffcd9a
Hephzibah Rajan <rajanhephzibah@gmail.com>
2021-10-21 19:06:32 -0400

    Small commented changes

--- server/geolocationAPI/geolocation.js
+++ server/geolocationAPI/geolocation.js
@@ -1,13 +1,22 @@
 var request = require('request');
 var config = require('../../config.js');
-
+//ith
 var requestGeolocation = function(location, callback) {
   var key = process.env.GEO_API || config.geolocationAPI;
-  var geoUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ location + '&key=' + key;
+  var geoUrl = 'https://api.radar.io/v1/users'+ location + '&key=' + key;
 
   request(geoUrl, function (error, response, body) {
      callback(JSON.parse(body));
   });
 }
+
+// var requestGeolocation = function(location, callback) {
+//   var key = process.env.GEO_API || config.geolocationAPI;
+//   var geoUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ location + '&key=' + key;
+//
+//   request(geoUrl, function (error, response, body) {
+//      callback(JSON.parse(body));
+//   });
+// }
 
 module.exports.requestGeolocation = requestGeolocation;
